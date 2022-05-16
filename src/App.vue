3dac59b40bfda98f4ec0effb334edea860eaf5ec
Djan <djan@MacBook-Pro-Djan.local>
2022-05-16 16:40:21 +0300

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+<!--Удалять наблюдение за обновлением состояние если удален счетчик-->
+<!--документация watch-->
+<!--Добавить Empty state при фильтрации если ничего нет-->
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
     <div v-if="!fetchedCoinList" class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
@@ -58,13 +61,23 @@
           Добавить
         </button>
       </section>
+      <hr class="w-full border-t border-gray-600 my-4" />
+      <div>
+        <button v-if="page > 1" @click="page-=1" class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Назад</button>
+        <button v-if="!lastPage" @click="page+=1" class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Вперед</button>
+        <span>Фильтр: </span>
+        <input
+            v-model="filterParam"
+            type="text"
+            class="pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md" placeholder="Что искать?"/>
+      </div>
       <template v-if="this.tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-              v-for="t in tickers"
+              v-for="t in filteredTickers"
               :key="t.name"
-              @click="selectTicker(t)"
+              @click="curTicker = t"
               :class="{
                 'border-4': t === curTicker
               }"
@@ -100,6 +113,7 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
+      <div v-if="!this.filteredTickers.length">Нет добавленных счетчиков или нет результатов удовлетворяющих фильтру</div>
       <section v-if="curTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
           {{curTicker.name}} - USD
@@ -150,20 +164,32 @@ export default {
   data(){
     return {
       ticker: '',
+      filterParam: '',
       maxInputs: 4,
       tAdded: false,
       tickers: [],
       curTicker: null,
       graph: [],
+      page: 1,
+      lastPage: false,
       fetchedCoinList: [],
       actualaizedCoinList:[]
     }
   },
-  methods: {
-    selectTicker (t){
-      this.curTicker = t;
+  computed: {
+    filteredTickers(){
+      let from = (this.page -1) * 6;
+      let to = this.page * 6;
+      let result = this.tickers
+          .filter(elem => elem.name.toUpperCase().includes(this.filterParam.toUpperCase()))
+      result.length <= to? this.lastPage = true : this.lastPage = false
+      return result.slice(from, to)
+    },
+    selectedTicker (){
       this.graph = []
     },
+  },
+  methods: {
     appendCoin (coin) {
       this.ticker = coin
       this.addTicker()
@@ -202,6 +228,9 @@ export default {
         this.curTicker = null
       }
       window.localStorage.setItem('tickerList', JSON.stringify(this.tickers));
+      if (this.filteredTickers.length === 0 && this.page > 1) {
+        this.page -= 1
+      }
     },
     normalizeGraph () {
       const maxVal = Math.max(...this.graph);
@@ -239,8 +268,24 @@ export default {
       }
     }
   },
+  watch:{
+    filterParam(){
+      this.page = 1;
+      window.history.pushState(null,'',`${window.location.pathname}?filter=${this.filterParam}&page=${this.page}`)
+    },
+    page(){
+      window.history.pushState(null,'',`${window.location.pathname}?filter=${this.filterParam}&page=${this.page}`)
+    }
+  },
   mounted() {
     this.tickers = JSON.parse(window.localStorage.getItem('tickerList'))
+    let windowData = Object.fromEntries(new URL(window.location).searchParams.entries())
+    if (windowData.filter) {
+      this.filterParam = windowData.filter
+    }
+    if (windowData.page) {
+      this.page = Number(windowData.page)
+    }
     if (this.tickers) {
       this.tickers.forEach(ticker => this.updateCurrency(ticker.name))
     }
