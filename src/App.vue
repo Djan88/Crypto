0ebb7b32929ca5e7cf0b5b64dd3e0bdfe69d18de
Ivan Avraamov <ivan.avraamov@gmail.com>
2022-05-22 09:36:32 +0300

    spliting fot components

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,9 @@
 <!-- Если монтеки нет в списке монет, то выделять ее красным -->
 <template>
 <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <div v-if="!fetchedCoinList" class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
-        <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-            </path>
-        </svg>
-    </div>
+    <app-paranja :fetchedCoinList="fetchedCoinList"></app-paranja>
     <div class="container">
-        <section>
-            <div class="flex">
-                <div class="max-w-xs">
-                    <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
-                    <div class="mt-1 relative rounded-md shadow-md">
-                        <input @keyup="inputHelper" @keypress.enter="addTicker" v-model="ticker" type="text" name="wallet" id="wallet" class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md" placeholder="Например DOGE" />
-                    </div>
-                    <div v-if="ticker" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
-                        <span @click="appendCoin(coinItem.Symbol)" v-for="coinItem in actualaizedCoinList" :key="coinItem.Id" class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-                            {{ coinItem.Symbol }}
-                        </span>
-                    </div>
-                    <div v-if="tAdded" class="text-sm text-red-600">Такой тикер уже добавлен</div>
-                </div>
-            </div>
-            <button @click="addTicker" type="button" :class="{
-            'disabled': !this.ticker.length
-          }" class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                <!-- Heroicon name: solid/mail -->
-                <svg class="-ml-0.5 mr-2 h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="#ffffff">
-                    <path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z">
-                    </path>
-                </svg>
-                Добавить
-            </button>
-        </section>
+        <app-ticker :tAdded="tAdded" :fetchedCoinList = "fetchedCoinList" @add-ticker="addTicker"/>
         <hr class="w-full border-t border-gray-600 my-4" />
         <div>
             <button v-if="page > 1" @click="page -= 1" class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Назад</button>
@@ -68,48 +37,34 @@
         </template>
         <div v-if="!this.filtredTickers.length">Нет добавленных счетчиков или нет результатов удовлетворяющих фильтру
         </div>
-        <section v-if="curTicker" class="relative">
-            <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-                {{ curTicker.name }} - USD
-            </h3>
-            <div class="flex items-end border-gray-600 border-b border-l h-64" ref="graph">
-                <div 
-                    v-for="(bar, idx) in normalizedGraph" 
-                    :key="idx" 
-                    :style="{ height: `${bar}%` }" 
-                    class="bg-purple-800 border w-10 h-24"
-                    ref="graphElem"></div>
-            </div>
-            <button @click="curTicker = null" type="button" class="absolute top-0 right-0">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="30" height="30" x="0" y="0" viewBox="0 0 511.76 511.76" style="enable-background:new 0 0 512 512" xml:space="preserve">
-                    <g>
-                        <path d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z" fill="#718096" data-original="#000000"></path>
-                    </g>
-                </svg>
-            </button>
-        </section>
+        <app-graph @remove-cur-ticker="curTicker = null" :curTicker="curTicker" :graphList="graphList"></app-graph>
     </div>
 </div>
 </template>
 
 <script>
 import {subscribeToTicker, unsubscribeFromTicker} from './api'
+import AppTicker from './components/AppTicker.vue'
+import AppParanja from './components/AppParanja.vue'
+import AppGraph from './components/AppGraph.vue'
 
 export default {
     name: 'App',
     data() {
         return {
-            ticker: '',
             filterParam: '',
-            maxInputs: 4,
             tAdded: false,
             tickers: [],
+            fetchedCoinList: [],
             curTicker: null,
             graphList: [],
             page: 1,
-            fetchedCoinList: [],
-            actualaizedCoinList: []
         }
+    },
+    components: {
+        AppTicker,
+        AppParanja,
+        AppGraph
     },
     mounted() {
         // Получаем тикеры из LS, если они есть
@@ -155,17 +110,6 @@ export default {
         hasNextPage() {
             return this.filtredTickers.length > this.toIndex
         },
-        // Форматирование графика
-        normalizedGraph() {
-            const maxVal = Math.max(...this.graphList);
-            const minVal = Math.min(...this.graphList);
-            if (maxVal === minVal) {
-                return this.graphList.map(() => 50)
-            }
-            return this.graphList.map(
-                price => 5 + ((price - minVal) * 95) / (maxVal - minVal)
-            )
-        },
         // Обновление параметров в URL
         pageStateOptions(){
             return {
@@ -183,18 +127,29 @@ export default {
         },
         tickers() {
             window.localStorage.setItem('tickerList', JSON.stringify(this.tickers));
-            if(this.curTicker){
-                // const value = this.tickers[this.curTicker.name]
-                // t.value = value ?? "-"
-                // if (this.curTicker && t.name === this.curTicker.name) {
-                // this.graph.push(value)
-            }
         },
         curTicker() {
             this.graphList = []
         }
     },
     methods: {
+        // Получение списка названий монет
+        fetchCoinNames() {
+            fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
+            .then((response) => {
+                return response.json()
+            })
+            .then((dataExchange) => {
+                for (let elem in dataExchange.Data) {
+                dataExchange.Data[elem].Id = Number(dataExchange.Data[elem].Id)
+                this.fetchedCoinList.push(dataExchange.Data[elem])
+                }
+                this.fetchedCoinList.sort((a, b) => {
+                    return a.Symbol.length - b.Symbol.length
+                })
+            })
+            this.$emit('coin-list', this.fetchedCoinList)
+        },
         calculateMaxGraphElements(){
             if (this.$refs.graphElem) {
                 return this.$refs.graph.offsetWidth / this.$refs.graphElem[0].offsetWidth
@@ -214,10 +169,6 @@ export default {
             }
             this.calculateMaxGraphElements()
         },
-        appendCoin(coin) {
-            this.ticker = coin
-            this.addTicker()
-        },
         // Приведение курса к нормальному виду
         formatedPrice(price){
             if (!price || price === '-') {
@@ -225,58 +176,23 @@ export default {
             }
             return price > 1 ? price.toFixed(2) : price.toPrecision(2)
         },
-        // Обновление криптовалют
-        // async updateCurrencys() {
-            // if(!this.tickers.length){
-            //     return
-            // }
-            // const parsedData = await loadTickers(this.tickers.map(t => t.name))
-            // this.tickers.forEach(t => {
-                // const value = Number(parsedData[t.name])
-                // t.value = value ?? "-"
-                // if (this.curTicker && t.name === this.curTicker.name) {
-                //     this.graph.push(value)
-                // }
-            // })
-        // },
-        // Получение списка названий криптовалют
-        fetchCoinNames() {
-            fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
-                .then((response) => {
-                    return response.json()
-                })
-                .then((dataExchange) => {
-                    for (let elem in dataExchange.Data) {
-                        dataExchange.Data[elem].Id = Number(dataExchange.Data[elem].Id)
-                        this.fetchedCoinList.push(dataExchange.Data[elem])
-                    }
-                    this.fetchedCoinList.sort((a, b) => {
-                        return a.Symbol.length - b.Symbol.length
-                    })
-                })
-        },
-        // Добавление тикера
-        addTicker() {
+        addTicker(ticker) {
             const newTicker = {
-                name: this.ticker.toUpperCase(),
+                name: ticker.toUpperCase(),
                 value: '-',
                 isExist: true
             };
             // Если такой тикер уже есть, выходим
             if (this.tickers.find(t => t.name === newTicker.name)) {
                 this.tAdded = true
-                this.ticker = ''
                 setTimeout(() => this.tAdded = false, 2000)
                 return
             }
-            if (this.ticker.length) {
-                this.tickers.push(newTicker)
-                this.tickers = [...this.tickers]
-                subscribeToTicker(newTicker.name, value => {
-                    this.updateTickers(newTicker.name, value)
-                })
-                this.ticker = ''
-            }
+            this.tickers.push(newTicker)
+            this.tickers = [...this.tickers]
+            subscribeToTicker(newTicker.name, value => {
+                this.updateTickers(newTicker.name, value)
+            })
         },
         // Удаление тикера
         removeTicker(n) {
@@ -290,20 +206,7 @@ export default {
                 this.page -= 1
             }
         },
-        // Подскази при вводе криптовалют
-        inputHelper() {
-            let counter = 0;
-            this.actualaizedCoinList = [];
-            for (let i = 0; i <= this.fetchedCoinList.length - 1; i++) {
-                if (this.fetchedCoinList[i].Symbol && this.fetchedCoinList[i].Symbol.toUpperCase().includes(this.ticker.toUpperCase())) {
-                    this.actualaizedCoinList.push(this.fetchedCoinList[i])
-                    counter++
-                }
-                if (counter === this.maxInputs) {
-                    break;
-                }
-            }
-        }
+        
     }
 }
 </script>
---
+++ src/components/AppTicker.vue
@@ -0,0 +1,95 @@
+<template>
+  <section>
+    <div class="flex">
+      <div class="max-w-xs">
+        <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
+        <div class="mt-1 relative rounded-md shadow-md">
+          <input 
+            @keyup="inputHelper" 
+            @keypress.enter="addTicker"
+            v-model="ticker" 
+            type="text" 
+            name="wallet" 
+            id="wallet"
+            class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            placeholder="Например DOGE" />
+        </div>
+        <div 
+          v-if="ticker" 
+          class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+          <span 
+            @click="appendCoin(coinItem.Symbol)" 
+            v-for="coinItem in actualaizedCoinList"
+            :key="coinItem.Id"
+            class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+            {{ coinItem.Symbol }}
+          </span>
+        </div>
+        <div v-if="this.tAdded === true" class="text-sm text-red-600">Такой тикер уже добавлен</div>
+      </div>
+    </div>
+    <app-button
+      @click="addTicker" 
+      type="button" 
+      :class="{
+        'disabled': !this.ticker.length
+      }">
+    </app-button>
+  </section>
+</template>
+<script>
+import AppButton from './AppButton.vue'
+
+export default {
+  components: {
+    AppButton
+  },
+  props: {
+    tAdded: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    fetchedCoinList: {
+      type: Array
+    }
+  },
+  emits: {
+    "add-ticker": value => typeof value === 'string' && value.length > 0
+  },
+  data() {
+    return {
+      ticker: '',
+      maxInputs: 4,
+      actualaizedCoinList: []
+    }
+  },
+  methods: {
+    // Подсказки при вводе криптовалют
+    inputHelper() {
+      let counter = 0;
+      this.actualaizedCoinList = [];
+      for (let i = 0; i <= this.fetchedCoinList.length - 1; i++) {
+        if (this.fetchedCoinList[i].Symbol && this.fetchedCoinList[i].Symbol.toUpperCase().includes(this.ticker.toUpperCase())) {
+            this.actualaizedCoinList.push(this.fetchedCoinList[i])
+            counter++
+        }
+        if (counter === this.maxInputs) {
+            break;
+        }
+      }
+    },
+    addTicker() {
+      if (this.ticker.length === 0) {
+        return;
+      }
+      this.$emit('add-ticker', this.ticker)
+      if (this.ticker.length) {
+        this.ticker = ''
+      }
+    },
+  },
+}
+</script>
+<style>
+</style>
